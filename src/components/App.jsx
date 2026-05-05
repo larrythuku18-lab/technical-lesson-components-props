a68c9ab79f36465ba41f1495d735afbf80d0951b
Larry Thuku <larrythuku18@gmail.com>
2026-05-05 12:54:04 +0300

    Importes details from Article.jsx to Articlelist.jsx and created a main tag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import blog from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 function App() {
   return (
     <div>
       {/* add the <Navbar> component here */}
+      <Navbar/>
       {/* add the <Home> component here */}
+      <Home  /> 
       {/* add your <About> component here */}
+      <About image={blog.image} about={blog.description} />
+      {/* add the <ArticleList> component here */}
+      <ArticleList posts={blog.posts} />\
     </div>
   );
 }
